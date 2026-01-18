59357c75bc1dd041d71f5402291b3db4f03c692a
Luis <Luisnavarr1999@gmail.com>
2026-01-18 05:30:14 -0300

    agregando informacion y configuraciones adicionales

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ document.querySelector('#app').innerHTML = `
       <p class="subtitle">Estudiante de Ingeniería en Informática</p>
 
       <div class="links">
-        <a class="btn github" href="https://github.com/TUUSUARIO" target="_blank" rel="noreferrer">
+        <a class="btn github" href="https://github.com/Luisnavarr1999" target="_blank" rel="noreferrer">
           <svg viewBox="0 0 24 24" aria-hidden="true">
             <path d="M12 .5C5.73.5.5 5.74.5 12.02c0 5.11 3.29 9.44 7.86 10.97.58.11.79-.25.79-.56v-2c-3.2.7-3.87-1.38-3.87-1.38-.53-1.35-1.29-1.71-1.29-1.71-1.05-.72.08-.71.08-.71 1.16.08 1.77 1.2 1.77 1.2 1.03 1.77 2.71 1.26 3.37.96.1-.75.4-1.26.72-1.55-2.55-.29-5.23-1.28-5.23-5.68 0-1.26.45-2.29 1.19-3.1-.12-.29-.52-1.45.11-3.02 0 0 .97-.31 3.18 1.18a10.9 10.9 0 0 1 5.8 0c2.21-1.49 3.18-1.18 3.18-1.18.63 1.57.23 2.73.11 3.02.74.81 1.19 1.84 1.19 3.1 0 4.41-2.69 5.38-5.25 5.67.41.36.78 1.07.78 2.15v3.19c0 .31.21.68.8.56 4.56-1.53 7.85-5.86 7.85-10.97C23.5 5.74 18.27.5 12 .5z"/>
           </svg>
